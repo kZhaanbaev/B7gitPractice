784f8e303eff5c28950b6dae433ede21928094f9
Daria <daria.trabsky@gmail.com>
2023-03-04 09:59:17 -0500

    second commit

--- src/test/java/DariaT.java
+++ src/test/java/DariaT.java
@@ -2,5 +2,6 @@ public class DariaT {
     public static void main(String[] args) {
         System.out.println("Hello Daria");
         System.out.println("Rise and shine!");
+        System.out.println("I need another cup of coffee))!");
     }
 }

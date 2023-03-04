9ff78bd83cd06c17bb8dca3e1190e8c1902c8572
Lisa <lisachernina@MacBook-Air.lan>
2023-03-04 06:40:08 -0800

    added new line

--- src/test/java/DariiaI.java
+++ src/test/java/DariiaI.java
@@ -1,5 +1,6 @@
 public class DariiaI {
     public static void main(String[] args) {
-        System.out.println("Hello Dariia???g");
+        System.out.println("Hello Dariia???");
+        System.out.println("have a good day");
     }
 }

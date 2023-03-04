bb14c72285641a5348165f9b6aaf37f3e8bfa7ed
ksenia <kseniawarda@gmail.com>
2023-03-03 21:46:07 -0700

    resolved conflict

--- src/test/java/Ksenia.java
+++ src/test/java/Ksenia.java
@@ -1,9 +1,7 @@
 public class Ksenia {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("Hello Ksenia???");
-=======
-        System.out.println("Hello Ksenia!!!");
->>>>>>> master
+
     }
 }
